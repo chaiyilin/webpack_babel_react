358b5d87af0e5fe0d65212b153a6b68edc8c5b71
yilin chai <chaiyilin@hotmail.com>
2017-04-19 20:44:30 +1000

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
@@ -5,9 +6,9 @@ var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
     //webpack
     devtool: 'eval-source-map',
-    entry: __dirname + '/src/index.js',
+    entry: path.join(__dirname + '/src/index.js'),
     output: {
-        path: __dirname + '/dist',
+        path: path.join(__dirname + '/dist'),
         filename: "bundle-[hash].js"
     },
     //Loaders
@@ -27,7 +28,7 @@ module.exports = {
     plugins: [
         new webpack.BannerPlugin("Copyright Flying Unicorns inc."),
         new HtmlWebpackPlugin({
-            template: __dirname + "/src/index.tmpl.html"
+            template: path.join(__dirname + "/src/index.tmpl.html")
         }),
         //new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin(),
@@ -35,7 +36,7 @@ module.exports = {
     ],
     //webpack-dev-server
     devServer: {
-        contentBase: __dirname + "/dist",
+        contentBase: path.join(__dirname + "/dist"),
         historyApiFallback: true,
         inline: true
     }
